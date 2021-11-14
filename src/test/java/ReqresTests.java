62bbdd505215bc7c34cdb2e9a774bebfa2c16c82
ShevkoplyasDarya <shevkoplyund@gmail.com>
2021-11-14 15:52:14 +0300

    added api tests for https://reqres.in/

---
+++ src/test/java/ReqresTests.java
@@ -0,0 +1,136 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresTests {
+
+    @Test
+    void successfulRegister() {
+        given()
+                .contentType(JSON)
+                .body("{\n" +
+                        "    \"email\": \"eve.holt@reqres.in\",\n" +
+                        "    \"password\": \"pistol\"\n" +
+                        "}")
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"))
+                .body("id", is(4));
+
+    }
+
+    @Test
+    void unsuccessfulRegister() {
+        given()
+                .contentType(JSON)
+                .body("{\n" +
+                        "    \"email\": \"sydney@fife\"\n" +
+                        "}")
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+
+    }
+
+    @Test
+    void successfulLogin() {
+        given()
+                .contentType(JSON)
+                .body("{\"email\": \"eve.holt@reqres.in\"," +
+                        "\"password\": \"cityslicka\"}")
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void unsuccessfulLogin() {
+        given()
+                .contentType(JSON)
+                .body("{\"email\": \"peter@klaven\"}")
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void getSingleUser() {
+        given()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"))
+                .body("data.first_name", is("Janet"))
+                .body("data.last_name", is("Weaver"))
+                .body("data.avatar", is("https://reqres.in/img/faces/2-image.jpg"))
+                .body("support.url", is("https://reqres.in/#support-heading"))
+                .body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
+    }
+
+    @Test
+    void successfulUpdateUser() {
+        given()
+                .contentType(JSON)
+                .body("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"zion resident\"\n" +
+                        "}")
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void successfulPatchUser() {
+        given()
+                .contentType(JSON)
+                .body("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"zion resident\"\n" +
+                        "}")
+                .when()
+                .patch("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void deleteUser() {
+        given()
+                .contentType(JSON)
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    void unsuccessfulGetResource() {
+        given()
+                .contentType(JSON)
+                .when()
+                .get("https://reqres.in/api/unknown/23")
+                .then()
+                .statusCode(404);
+    }
+
+
+}
+
+
